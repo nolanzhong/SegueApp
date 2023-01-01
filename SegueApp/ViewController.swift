e0b96eaa3970c04a04990540fd897b67cae1b056
nolanzhong <nolan.zhong@duke.edu>
2023-01-01 17:09:27 -0500

    Initial Commit

---
+++ SegueApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SegueApp
+//
+//  Created by Nolan Zhong on 1/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
